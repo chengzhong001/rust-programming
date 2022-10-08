c9c05c9406f6929c14f59893e0f1298db47c60ec
azhong <zczwzw@163.com>
2022-10-08 22:23:25 +0800

    know about ownership

--- 04-ownership/src/main.rs
+++ 04-ownership/src/main.rs
@@ -1,7 +1,71 @@
+#![allow(unused)]
 fn main() {
-    let s = "hello";{
-        let s = "world";
-        println!("s value is {}", s);
+    //什么是所有权
+    {
+        let s = "hello";
+        {
+            let s = "world";
+        }
+        let s = String::from("hello");
+
+        let mut s = String::from("hello");
+        s.push_str(", world!");
+        println!("{}", s);
+        let x = 5;
+        let y = x;
+        println!("x is {}, y is {}", x, y);
+
+        let s1 = String::from("hello");
+        let s2 = s1; // s1 move to s2
+        println!("s1 = {}", s2);
+
+        let s1 = String::from("hello");
+        let s2 = s1.clone();
+        println!("s1 = {}, s2 = {}", s1, s2);
+
+        let s = String::from("hello");
+        takes_ownership(s);
+
+        let x = 5;
+        makes_copy(x);
+
+        let s1 = gives_ownership(); // gives_ownership 将返回值
+                                    // 移给 s1
+
+        let s2 = String::from("hello"); // s2 进入作用域
+
+        let s3 = takes_and_gives_back(s2); // s2 被移动到
+                                           // takes_and_gives_back 中,
+                                           // 它也将返回值移给 s3
+
+        let s1 = String::from("hello");
+        let (s2, len) = calculate_length(s1);
+        println!("The length of '{}' is {}.", s2, len);
     }
-    println!("s value is {}", s);
+}
+
+fn takes_ownership(some_string: String) {
+    // some_string 进入作用域
+    println!("{}", some_string);
+} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+
+fn makes_copy(some_integer: i32) {
+    // some_integer 进入作用域
+    println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string 进入作用域
+
+    a_string // 返回 a_string 并移出给调用的函数
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    return (s, length);
 }
