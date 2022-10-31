8ce5aff682d10890d953d78fb810facac3a49bd1
azhong <zczwzw@163.com>
2022-10-31 21:54:30 +0800

    lifetime

---
+++ 10-generic_traits_lifetime/src/bin/lifetime.rs
@@ -0,0 +1,71 @@
+#![allow(unused)]
+use std::fmt::Display;
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest2(x: String, y: String) -> String {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(&string1.as_str(), &string2);
+    println!("result is {}", result);
+
+    {
+        let string3 = "xxxx";
+        let result = longest(&string1.as_str(), &string3);
+        println!("result is {}", result);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{:#?}", i);
+    dbg!(i);
+    let a: &str;
+    {
+        a = "hello";
+    }
+    println!("{}", a);
+}
